a19626fa5b00599f8d02c8d314f52bdd0cbce759
Daniel Wahl <contact@danielwahl.lu>
2021-11-12 14:06:02 +0100

    Added heading-tag setting

--- components/Items/Heading.tsx
+++ components/Items/Heading.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 import ContentComponentData from "../../types/component/ContentComponentData";
 
-const Heading = ({ settings }: ContentComponentData) => {
+const Heading: React.FC<ContentComponentData> = ({ settings }) => {
+    const { text, tag } = settings;
+
     return (
-        <div className="component-text">
-            <h2 dangerouslySetInnerHTML={{ __html: settings?.text }} />
+        <div className="component-heading">
+            {tag && tag === "h1" && (
+                <h1
+                    className={settings?.class ?? ""}
+                    dangerouslySetInnerHTML={{ __html: text }}
+                />
+            )}
+            {tag && tag === "h2" && (
+                <h2
+                    className={settings?.class ?? ""}
+                    dangerouslySetInnerHTML={{ __html: text }}
+                />
+            )}
+            {tag && tag === "h3" && (
+                <h3
+                    className={settings?.class ?? ""}
+                    dangerouslySetInnerHTML={{ __html: text }}
+                />
+            )}
+            {tag && tag === "h4" && (
+                <h4
+                    className={settings?.class ?? ""}
+                    dangerouslySetInnerHTML={{ __html: text }}
+                />
+            )}
+            {!tag && (
+                <h2
+                    className={settings?.class ?? ""}
+                    dangerouslySetInnerHTML={{ __html: text }}
+                />
+            )}
         </div>
     );
 };
